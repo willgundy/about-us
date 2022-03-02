efe818b3f18c7e9c356ebfbd56f41807a5171533
Will Gundy <willgunderson503@gmail.com>
2022-03-02 14:57:00 -0800

    Added event listener in app javascript file for selector

--- app.js
+++ app.js
@@ -13,3 +13,14 @@ const hobbies = document.querySelector('.hobbies');
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
+selectorElement.addEventListener('change', (event) => {
+    const selectedName = event.target.value;
+    
+    if (selectedName === 'one') {
+        //come back and use javascript function for array position 0
+        console.log(selectedName);
+    } else {
+        //come back and use javascript function for array position 1
+        console.log(selectedName);
+    }
+});
