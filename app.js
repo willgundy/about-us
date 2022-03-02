caf68b5c7d070a58c3bd47a232b36d335834db4b
Will Gundy <willgunderson503@gmail.com>
2022-03-02 14:51:34 -0800

    Added query selectors for html elements in app.js file

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // import functions
-// small change
+import {people} from './data.js';
 
 // grab DOM elements
+const selectorElement = document.querySelector('.selector');
+const container = document.querySelector('.container');
+const nameHeader = document.querySelector('.name');
+const shortBio = document.querySelector('.short-bio');
+const age = document.querySelector('.age');
+const hobbies = document.querySelector('.hobbies');
 
 // set event listeners 
     // get user input
